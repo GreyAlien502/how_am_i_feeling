0687166de0e4c7d7579a85bf802e098e6f83eda5
GreyAlien502 <greyalien502@gmail.com>
2017-12-03 11:34:53 -0500

    forgot to commit the final changes./

--- extension/general/popup.js
+++ extension/general/popup.js
@@ -1,12 +1,20 @@
-//function render(){
-//	c3.generate({
-//		bindto: '#graph',
-//		x:"time",
-//		columns:[ times, moods ]
-//	});
-//}
-let port = chrome.runtime.connect({name:"moodTransfer"});
+let port = chrome.runtime.connect({ name: "moodTransfer" });
 port.postMessage("Commence emotion transfer.");
-port.onMessage.addListener( moodJSON =>
-	document.getElementById("text").innerHTML = moodJSON
-);
+port.onMessage.addListener(function (moodJSON) {
+	let parsedJson = JSON.parse(moodJSON);
+	var chart = c3.generate({
+		bindto: '#chart',
+		data: {
+			json: {
+				mood: parsedJson
+			}
+		
+		},
+		axis : {
+			y: {
+				min: -1,
+				max: 1
+			}
+		}
+	});
+});
